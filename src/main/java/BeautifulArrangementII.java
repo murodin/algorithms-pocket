f1a06b7b5da8bfacc53832c0fa0306bb3c889c69
Murat D <murat_dindar@yahoo.com>
2021-09-04 13:55:09 +0300

    Beautiful Arrangement II problem implemented.

---
+++ src/main/java/BeautifulArrangementII.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+public class BeautifulArrangementII {
+
+    /*
+        Given two integers n and k, construct a list answer that contains n different positive integers ranging from 1 to n and obeys the following requirement:
+
+        Suppose this list is answer = [a1, a2, a3, ... , an], then the list [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] has exactly k distinct integers.
+        Return the list answer. If there multiple valid answers, return any of them.
+
+        Example 1.
+        Input: n = 3, k = 1
+        Output: [1,2,3]
+        Explanation: The [1,2,3] has three different positive integers ranging from 1 to 3, and the [1,1] has exactly 1 distinct integer: 1
+
+        Example 2.
+        Input: n = 3, k = 2
+        Output: [1,3,2]
+        Explanation: The [1,3,2] has three different positive integers ranging from 1 to 3, and the [2,1] has exactly 2 distinct integers: 1 and 2.
+     */
+    public static void main(String[] args) {
+        Arrays.asList(Solution.constructArray(3, 1)).forEach(System.out::println);
+    }
+
+    static class Solution {
+        // Time: O(N)
+        // Space: O(N)
+        public static int[] constructArray(int n, int k) {
+            int[] result =new int[n];
+
+            int high = n , low = 1;
+            int index = 0;
+
+            result[index++] = low++;
+
+            boolean isHigh = false;
+
+            while(k>1){
+                result[index++] = high--;
+                k--;
+                isHigh = true;
+                if(k>1){
+                    result[index++] = low++;
+                    k--;
+                    isHigh = false;
+                }
+            }
+
+            //increasing or decreasing
+            while(index<n){
+                if(isHigh){
+                    result[index++] = high--;
+                }
+                else{
+                    result[index++] = low++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
