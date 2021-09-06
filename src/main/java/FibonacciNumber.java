2af89728bcd6f6ec6a9b9283366cf8a75e5627c4
Murat D <murat_dindar@yahoo.com>
2021-09-06 10:46:36 +0300

    Fibonacci Number problem implemented.

---
+++ src/main/java/FibonacciNumber.java
@@ -0,0 +1,42 @@
+public class FibonacciNumber {
+
+    /*
+        The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence,
+        such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+        F(0) = 0, F(1) = 1
+        F(n) = F(n - 1) + F(n - 2), for n > 1.
+        Given n, calculate F(n).
+
+        Example 1.
+        Input: n = 2
+        Output: 1
+        Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+        Example 2.
+        Input: n = 3
+        Output: 2
+        Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Fib Number:" + Solution.fib(5));
+    }
+
+    static class Solution {
+        // Time: O(N)
+        // Space: O(N)
+        static int[] dp = new int[31];
+
+        public static int fib(int n) {
+            if(n<=1) return n;
+
+            if(dp[n]>0){
+                return dp[n];
+            }
+
+            dp[n] = fib(n-1) + fib(n-2);
+            return dp[n];
+        }
+    }
+}
