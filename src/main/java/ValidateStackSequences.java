e6e36c4b4152041b6ed6e59f9b308bfcee8fb55c
Murat D <murat_dindar@yahoo.com>
2021-10-08 00:12:50 +0300

    Validate Stack Sequences problem implemented.

--- src/main/java/ValidateStackSequences.java
+++ src/main/java/ValidateStackSequences.java
@@ -31,6 +31,7 @@ public class ValidateStackSequences {
         public static boolean validateStackSequences(int[] pushed, int[] popped) {
             Stack<Integer> st = new Stack<>();
             int j = 0;
+
             for(int p : pushed) {
                 st.push(p);
                 while(!st.isEmpty() && j < popped.length && st.peek() == popped[j]) {
