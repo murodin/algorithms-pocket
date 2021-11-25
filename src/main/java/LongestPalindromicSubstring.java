0d3c8a1cee6cf038f3ad5aa3abb1ce63932e9290
Murat D <murat_dindar@yahoo.com>
2021-11-26 00:35:40 +0300

    Longest Palindromic Substring problem edited.

--- src/main/java/LongestPalindromicSubstring.java
+++ src/main/java/LongestPalindromicSubstring.java
@@ -19,12 +19,13 @@ public class LongestPalindromicSubstring {
      */
 
     public static void main(String[] args) {
-        System.out.println("Longest Palindromic Substring: " + Solution.longestPalindrome("babad"));
+        System.out.println("Solution I - Longest Palindromic Substring: " + Solution_I.longestPalindrome("babad"));
+        System.out.println("Solution II - Longest Palindromic Substring: " + Solution_II.longestPalindrome("babad"));
     }
 
     // Time: O(N)
     // Space: O(N)
-    static class Solution {
+    static class Solution_I {
         public static String longestPalindrome(String s) {
             int n=s.length();
             char[] newStr=new char[2*n+1];
@@ -70,6 +71,32 @@ public class LongestPalindromicSubstring {
 
             String st=new String(newStr);
             return st.substring(longestCenter-longestLength,longestCenter+longestLength).replace("#","");
+        }
+    }
+
+    static class Solution_II {
+        public static String longestPalindrome(String s) {
+            int start = 0, end = 0;
+            for(int i = 0; i < s.length(); i++){
+                int odd = expand(s,i,i);
+                int even = expand(s,i,i+1);
+                int len = Math.max(odd, even);
+                if(len > end-start){
+                    //Even len (6)-> 2 start --> i-2, end -> i+3
+                    //Odd len (5) -> 2 start i-2, end -> i+2
+                    start = i - (len-1)/2;
+                    end = i+ len/2;
+                }
+            }
+            return s.substring(start, end+1);
+        }
+
+        static int expand(String s, int left, int right){
+            while(left >= 0 && right <s.length() && s.charAt(left) == s.charAt(right)){
+                left--;
+                right++;
+            }
+            return right-left-1;
         }
     }
 }
