7cecb9eab7c13c9a59c6186d064ac6b52404a26f
murodin <murat_dindar@yahoo.com>
2021-12-30 00:42:03 +0300

    Check Array Formation Through Concatenation user problem edited.

--- src/main/java/CheckArrayFormationThroughConcatenation.java
+++ src/main/java/CheckArrayFormationThroughConcatenation.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class CheckArrayFormationThroughConcatenation {
+
     /*
         You are given an array of distinct integers arr and an array of integer arrays pieces,
         where the integers in pieces are distinct. Your goal is to form arr by concatenating the arrays in pieces in any order. However, you are not allowed to reorder the integers in each array pieces[i].
