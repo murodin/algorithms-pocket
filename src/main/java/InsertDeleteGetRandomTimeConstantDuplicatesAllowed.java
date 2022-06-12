77e7415cf7de5228f68145018cdac96cf1db2e53
murodin <murat_dindar@yahoo.com>
2022-06-12 14:17:49 +0300

    Insert Delete Get Random O(1) Duplicates Allowed implemented.

---
+++ src/main/java/InsertDeleteGetRandomTimeConstantDuplicatesAllowed.java
@@ -0,0 +1,142 @@
+import java.util.*;
+
+public class InsertDeleteGetRandomTimeConstantDuplicatesAllowed {
+
+    /*
+        RandomizedCollection is a data structure that contains a collection of numbers, possibly duplicates (i.e., a multiset).
+        It should support inserting and removing specific elements and also removing a random element.
+        Implement the RandomizedCollection class:
+        RandomizedCollection() Initializes the empty RandomizedCollection object.
+        bool insert(int val) Inserts an item val into the multiset, even if the item is already present. Returns true if the item is not present, false otherwise.
+        bool remove(int val) Removes an item val from the multiset if present. Returns true if the item is present, false otherwise. Note that if val has multiple occurrences in the multiset, we only remove one of them.
+        int getRandom() Returns a random element from the current multiset of elements. The probability of each element being returned is linearly related to the number of same values the multiset contains.
+        You must implement the functions of the class such that each function works on average O(1) time complexity.
+
+        Note: The test cases are generated such that getRandom will only be called if there is at least one item in the RandomizedCollection.
+
+        Example 1.
+        Input
+        ["RandomizedCollection", "insert", "insert", "insert", "getRandom", "remove", "getRandom"]
+        [[], [1], [1], [2], [], [1], []]
+        Output
+        [null, true, false, true, 2, true, 1]
+
+        Explanation
+            RandomizedCollection randomizedCollection = new RandomizedCollection();
+            randomizedCollection.insert(1);   // return true since the collection does not contain 1.
+                                              // Inserts 1 into the collection.
+            randomizedCollection.insert(1);   // return false since the collection contains 1.
+                                              // Inserts another 1 into the collection. Collection now contains [1,1].
+            randomizedCollection.insert(2);   // return true since the collection does not contain 2.
+                                              // Inserts 2 into the collection. Collection now contains [1,1,2].
+            randomizedCollection.getRandom(); // getRandom should:
+                                              // - return 1 with probability 2/3, or
+                                              // - return 2 with probability 1/3.
+            randomizedCollection.remove(1);   // return true since the collection contains 1.
+                                              // Removes 1 from the collection. Collection now contains [1,2].
+            randomizedCollection.getRandom(); // getRandom should return 1 or 2, both equally likely.
+
+
+        Constraints:
+
+        -231 <= val <= 231 - 1
+        At most 2 * 105 calls in total will be made to insert, remove, and getRandom.
+        There will be at least one element in the data structure when getRandom is called.
+     */
+
+    public static void main(String[] args) {
+        //Your RandomizedCollection object will be instantiated and called as such:
+        RandomizedCollection_I obj1 = new RandomizedCollection_I();
+        boolean param_1 = obj1.insert(0);
+        boolean param_2 = obj1.remove(1);
+        int param_3 = obj1.getRandom();
+        System.out.println("Solution I:" + param_3);
+
+        RandomizedCollection_II obj2 = new RandomizedCollection_II();
+        boolean param_4 = obj2.insert(0);
+        boolean param_5 = obj2.remove(1);
+        int param_6 = obj2.getRandom();
+        System.out.println("Solution I:" + param_6);
+    }
+
+    // Time:
+    // Space:
+    static class RandomizedCollection_I {
+        private HashMap<Integer, Integer> map;
+        private ArrayList<Integer> list ;
+        public RandomizedCollection_I() {
+            map = new HashMap<>();
+            list = new ArrayList<>();
+        }
+
+        public boolean insert(int val) {
+            list.add(val);
+            if(!map.containsKey(val)){
+                map.put(val,1);
+                return true;
+            }else{
+                map.put(val, map.get(val) + 1);
+                return false;
+            }
+        }
+
+        public boolean remove(int val) {
+            if(!map.containsKey(val)) return false;
+            else{
+
+                int index = list.indexOf(val);
+                list.remove(index);
+
+                if(map.get(val) == 1) map.remove(val);
+                else map.put(val, map.get(val)-1);
+                return true;
+            }
+        }
+
+        public int getRandom() {
+            int rand_int = (int)Math.floor(Math.random()*(list.size()));
+            return list.get(rand_int);
+        }
+    }
+
+    // Time:
+    // Space:
+    static class RandomizedCollection_II {
+        Map<Integer, Queue<Integer>> m;
+        List<Integer> l;
+        Random rand;
+        public RandomizedCollection_II() {
+            m = new HashMap<>();
+            l = new ArrayList<>();
+            rand = new Random();
+        }
+
+        public boolean insert(int val) {
+            boolean contains = m.containsKey(val);
+            if(!contains) m.put(val,new PriorityQueue<>(Collections.reverseOrder()));
+            m.get(val).offer(l.size());
+            l.add(val);
+            return !contains;
+        }
+
+        public boolean remove(int val) {
+            boolean contains = m.containsKey(val);
+            if(contains) {
+                int ind = m.get(val).poll(), n=l.size()-1;
+                if(ind!=n) {
+                    Queue<Integer> ts = m.get(l.get(n));
+                    ts.poll();
+                    ts.offer(ind);
+                    l.set(ind,l.get(n));
+                }
+                if(m.get(val).size() == 0)  m.remove(val);
+                l.remove(n);
+            }
+            return contains;
+        }
+
+        public int getRandom() {
+            return l.get(rand.nextInt(l.size()));
+        }
+    }
+}
