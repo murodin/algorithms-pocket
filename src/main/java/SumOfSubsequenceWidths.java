4542877e15ac2b5c59bb9e7ad7988b3267e50fb9
murodin <murat_dindar@yahoo.com>
2022-06-23 22:51:45 +0300

    Sum Of Subsequence Widths implemented.

---
+++ src/main/java/SumOfSubsequenceWidths.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class SumOfSubsequenceWidths {
+    /*
+        The width of a sequence is the difference between the maximum and minimum elements in the sequence.
+        Given an array of integers nums, return the sum of the widths of all the non-empty subsequences of nums. Since the answer may be very large, return it modulo 109 + 7.
+
+        A subsequence is a sequence that can be derived from an array by deleting some or no elements without changing the order of the remaining elements.
+        For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+
+        Example 1.
+        Input: nums = [2,1,3]
+        Output: 6
+        Explanation: The subsequences are [1], [2], [3], [2,1], [2,3], [1,3], [2,1,3].
+        The corresponding widths are 0, 0, 0, 1, 1, 2, 2.
+        The sum of these widths is 6.
+        Example 2.
+        Input: nums = [2]
+        Output: 0
+
+
+        Constraints:
+
+        1 <= nums.length <= 105
+        1 <= nums[i] <= 105
+     */
+
+    public static void main(String[] args) {
+        int[] nums = {2,1,3};
+        System.out.println("Solution: " + Solution.sumSubseqWidths(nums));
+    }
+
+    // Time: O(NLogN)
+    // Space: O(1)
+    static class Solution {
+        public static int sumSubseqWidths(int[] A) {
+            Arrays.sort(A);
+            long c = 1, res = 0, mod = (long)1e9 + 7;
+            for (int i = 0, n = A.length; i < n; ++i, c = c * 2 % mod)
+                res = (res + A[i] * c - A[n - i - 1] * c) % mod;
+            return (int)((res + mod) % mod);
+        }
+    }
+}
