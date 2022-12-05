4b8312113f44abcf60c2403736c98ab004f3ccdc
murodin <murat_dindar@yahoo.com>
2022-12-06 00:38:33 +0300

    Expressive Words implemented.

---
+++ src/main/java/ExpressiveWords.java
@@ -0,0 +1,56 @@
+public class ExpressiveWords {
+    /*
+        Sometimes people repeat letters to represent extra feeling. For example:
+        "hello" -> "heeellooo"
+        "hi" -> "hiiii"
+        In these strings like "heeellooo", we have groups of adjacent letters that are all the same: "h", "eee", "ll", "ooo".
+
+        You are given a string s and an array of query strings words.
+         A query word is stretchy if it can be made to be equal to s by any number of applications of the following extension operation: choose a group consisting of characters c,
+         and add some number of characters c to the group so that the size of the group is three or more.
+
+        For example, starting with "hello", we could do an extension on the group "o" to get "hellooo", but we cannot get "helloo" since the group "oo" has a size less than three.
+         Also, we could do another extension like "ll" -> "lllll" to get "helllllooo". If s = "helllllooo", then the query word "hello" would be stretchy because of these two extension operations: query = "hello" -> "hellooo" -> "helllllooo" = s.
+        Return the number of query strings that are stretchy.
+
+
+        Example 1.
+        Input: s = "heeellooo", words = ["hello", "hi", "helo"]
+        Output: 1
+        Explanation:
+        We can extend "e" and "o" in the word "hello" to get "heeellooo".
+        We can't extend "helo" to get "heeellooo" because the group "ll" is not size 3 or more.
+        Example 2.
+        Input: s = "zzzzzyyyyy", words = ["zzyy","zy","zyy"]
+        Output: 3
+
+
+        Constraints:
+
+        1 <= s.length, words.length <= 100
+        1 <= words[i].length <= 100
+        s and words[i] consist of lowercase letters.
+     */
+    public static void main(String[] args) {
+        System.out.println("Solution: " + Solution.expressiveWords("zzzzzyyyyy", new String[] {"zzyy","zy","zyy"}));
+    }
+
+    // Time: O(NxM), where N length of String; M length of String Array
+    // Space: O(1)
+    static class Solution {
+        public static int expressiveWords(String S, String[] words) {
+            int res = 0;
+            for (String W : words) if (check(S, W)) res++;
+            return res;
+        }
+        public static boolean check(String S, String W) {
+            int n = S.length(), m = W.length(), j = 0;
+            for (int i = 0; i < n; i++)
+                if (j < m && S.charAt(i) == W.charAt(j)) j++;
+                else if (i > 1 && S.charAt(i) == S.charAt(i - 1) && S.charAt(i - 1) == S.charAt(i - 2));
+                else if (0 < i && i < n - 1 && S.charAt(i - 1) == S.charAt(i) && S.charAt(i) == S.charAt(i + 1));
+                else return false;
+            return j == m;
+        }
+    }
+}
