4fd1a3345e598fd857252f4335e57800888e0f84
murodin <murat_dindar@yahoo.com>
2022-12-29 22:57:09 +0300

    SingleThreadedCPU - Solution II added.

--- src/main/java/SingleThreadedCPU.java
+++ src/main/java/SingleThreadedCPU.java
@@ -47,12 +47,13 @@ public class SingleThreadedCPU {
 
     public static void main(String[] args) {
         int[][] tasks = {{7,10},{7,12},{7,5},{7,4},{7,2}};
-        System.out.println("Solution: " + Arrays.toString(Solution.getOrder(tasks)));
+        System.out.println("Solution I: " + Arrays.toString(SolutionI.getOrder(tasks)));
+        System.out.println("Solution II: " + Arrays.toString(SolutionII.getOrder(tasks)));
     }
 
     // Time: O(NLogN)
     // Space: O(N)
-    static class Solution {
+    static class SolutionI {
         public static int[] getOrder(int[][] tasks1) {
             int n = tasks1.length;
 
@@ -105,6 +106,59 @@ public class SingleThreadedCPU {
             }
 
             return taskOrderResult;
+        }
+    }
+
+    // Time: O(NLogN)
+    // Space: O(N)
+    static class SolutionII {
+        public static int[] getOrder(int[][] tasks) {
+            int n = tasks.length;
+            Task [] arr = new Task[n];
+            for(int i = 0 ;i<n;i++){
+                arr[i] = new Task(i, tasks[i][0],tasks[i][1]);
+            }
+
+            Arrays.sort(arr, (a,b)->{
+                return Integer.compare(a.enqueueTime,b.enqueueTime);
+            });
+
+            PriorityQueue<Task> p = new PriorityQueue<>((a,b)->{
+                if(a.processingTime == b.processingTime){
+                    return Integer.compare(a.idx,b.idx);
+                }
+                return Integer.compare(a.processingTime,b.processingTime);
+            });
+
+            int[] ans = new int[n];
+            int ansIdx = 0;
+            int taskIdx = 0;
+            int curTime= 0;
+
+            while(ansIdx < n){
+                while(taskIdx < n && arr[taskIdx].enqueueTime <= curTime){
+                    p.offer(arr[taskIdx++]);
+                }
+                if(p.isEmpty()){
+                    curTime = arr[taskIdx].enqueueTime;
+                } else{
+                    curTime += p.peek().processingTime;
+                    ans[ansIdx++] = p.poll().idx;
+                }
+            }
+            return ans;
+        }
+
+        static class Task {
+            int idx;
+            int enqueueTime;
+            int processingTime;
+
+            Task(int idx , int en , int pro){
+                this.idx = idx;
+                this.enqueueTime = en;
+                this.processingTime = pro;
+            }
         }
     }
 }
