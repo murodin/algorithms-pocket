3c949c4db660c3c709294892064bdfc47bcc4c2b
murodin <murat_dindar@yahoo.com>
2023-02-02 23:01:30 +0300

    Find Greatest Common Divisor Of Array implemented.

---
+++ src/main/java/FindGreatestCommonDivisorOfArray.java
@@ -0,0 +1,60 @@
+public class FindGreatestCommonDivisorOfArray {
+    /*
+        Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+        The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+
+        Example 1.
+        Input: nums = [2,5,6,9,10]
+        Output: 2
+        Explanation:
+        The smallest number in nums is 2.
+        The largest number in nums is 10.
+        The greatest common divisor of 2 and 10 is 2.
+        Example 2.
+        Input: nums = [7,5,6,8,3]
+        Output: 1
+        Explanation:
+        The smallest number in nums is 3.
+        The largest number in nums is 8.
+        The greatest common divisor of 3 and 8 is 1.
+        Example 3.
+        Input: nums = [3,3]
+        Output: 3
+        Explanation:
+        The smallest number in nums is 3.
+        The largest number in nums is 3.
+        The greatest common divisor of 3 and 3 is 3.
+
+
+        Constraints:
+
+        2 <= nums.length <= 1000
+        1 <= nums[i] <= 1000
+     */
+    public static void main(String[] args) {
+        System.out.println("Solution: " + Solution.findGCD(new int[]{2,5,6,9,10}));
+    }
+
+    // Time: O(N)
+    // Space: O(1)
+    static class Solution {
+        public static int findGCD(int[] nums) {
+            int minN = nums[0];
+            int maxN = nums[0];
+            for (int i = 0; i != nums.length; ++i) {
+                if (minN > nums[i]) minN = nums[i];
+                if (maxN < nums[i]) maxN = nums[i];
+            }
+            // Euclidean Algorithm
+            while (minN != maxN) {
+                if (minN > maxN) {
+                    minN -= maxN;
+                } else {
+                    maxN -= minN;
+                }
+            }
+            return minN;
+        }
+    }
+}
