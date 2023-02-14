626b38412d1d6b12a0ac4f8dfd5de0fd67ccc247
murodin <murat_dindar@yahoo.com>
2023-02-15 00:42:58 +0300

    Add Binary implemented.

---
+++ src/main/java/AddBinary.java
@@ -0,0 +1,50 @@
+public class AddBinary {
+    /*
+        Given two binary strings a and b, return their sum as a binary string.
+
+        Example 1.
+        Input: a = "11", b = "1"
+        Output: "100"
+        Example 2.
+        Input: a = "1010", b = "1011"
+        Output: "10101"
+
+
+        Constraints:
+
+        1 <= a.length, b.length <= 104
+        a and b consist only of '0' or '1' characters.
+        Each string does not contain leading zeros except for the zero itself.
+     */
+    public static void main(String[] args) {
+        System.out.println("Solution: " + Solution.addBinary("1010", "1011"));
+    }
+
+    // Time: O(max(M, N)), where M and N are the lengths of the binary strings a and b, respectively.
+    // Space: O(max(M, N)), where M and N are the lengths of the binary strings a and b, respectively.
+    static class Solution {
+        public static String addBinary(String a, String b) {
+            StringBuilder res = new StringBuilder();
+            int i = a.length() - 1;
+            int j = b.length() - 1;
+            int carry = 0;
+            while(i >= 0 || j >= 0){
+                int sum = carry;
+                if(i >= 0){
+                    sum += a.charAt(i) - '0';
+                    i--;
+                }
+                if(j >= 0){
+                    sum += b.charAt(j) - '0';
+                    j--;
+                }
+                carry = sum > 1 ? 1 : 0;
+                res.append(sum % 2);
+            }
+            if(carry != 0){
+                res.append(carry);
+            }
+            return res.reverse().toString();
+        }
+    }
+}
