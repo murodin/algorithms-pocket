f9f3c7711abc73cf844ed097c962e1664d8181a7
murodin <murat_dindar@yahoo.com>
2023-03-02 00:03:44 +0300

    Smallest Rotation With Highest Score implemented.

---
+++ src/main/java/SmallestRotationWithHighestScore.java
@@ -0,0 +1,58 @@
+public class SmallestRotationWithHighestScore {
+    /*
+        You are given an array nums. You can rotate it by a non-negative integer k so that the array becomes [nums[k], nums[k + 1], ... nums[nums.length - 1], nums[0], nums[1], ..., nums[k-1]].
+        Afterward, any entries that are less than or equal to their index are worth one point.
+        For example, if we have nums = [2,4,1,3,0], and we rotate by k = 2, it becomes [1,3,0,2,4].
+        This is worth 3 points because 1 > 0 [no points], 3 > 1 [no points], 0 <= 2 [one point], 2 <= 3 [one point], 4 <= 4 [one point].
+        Return the rotation index k that corresponds to the highest score we can achieve if we rotated nums by it. If there are multiple answers, return the smallest such index k.
+
+        Example 1.
+        Input: nums = [2,3,1,4,0]
+        Output: 3
+        Explanation: Scores for each k are listed below:
+        k = 0,  nums = [2,3,1,4,0],    score 2
+        k = 1,  nums = [3,1,4,0,2],    score 3
+        k = 2,  nums = [1,4,0,2,3],    score 3
+        k = 3,  nums = [4,0,2,3,1],    score 4
+        k = 4,  nums = [0,2,3,1,4],    score 3
+        So we should choose k = 3, which has the highest score.
+        Example 2.
+        Input: nums = [1,3,0,2,4]
+        Output: 0
+        Explanation: nums will always have 3 points no matter how it shifts.
+        So we will choose the smallest k, which is 0.
+
+
+        Constraints:
+
+        1 <= nums.length <= 105
+        0 <= nums[i] < nums.length
+     */
+    public static void main(String[] args) {
+        System.out.println("Solution: " + Solution.bestRotation(new int[]{2,3,1,4,0}));
+    }
+
+    // Time: O(N)
+    // Space: O(N)
+   static class Solution {
+        public static int bestRotation(int[] A) {
+            int n = A.length;
+            int[] a = new int[n];
+            for (int i = 0; i < A.length; i++) {
+                a[(i + 1) % n]++;
+                a[(i + 1 - A[i] + n) % n]--;
+            }
+            int count = 0;
+            int maxCount = -1;
+            int res = 0;
+            for (int i = 0; i < n; i++) {
+                count += a[i];
+                if (count > maxCount) {
+                    res = i;
+                    maxCount = count;
+                }
+            }
+            return res;
+        }
+    }
+}
