bfcadb43faddac6ee11ebdf2163934414452b796
murodin <murat_dindar@yahoo.com>
2023-03-28 00:35:54 +0300

    Cherry Pickup implemented.

---
+++ src/main/java/CherryPickup.java
@@ -0,0 +1,69 @@
+public class CherryPickup {
+    /*
+        You are given an n x n grid representing a field of cherries, each cell is one of three possible integers.
+        0 means the cell is empty, so you can pass through,
+        1 means the cell contains a cherry that you can pick up and pass through, or
+        -1 means the cell contains a thorn that blocks your way.
+        Return the maximum number of cherries you can collect by following the rules below:
+        Starting at the position (0, 0) and reaching (n - 1, n - 1) by moving right or down through valid path cells (cells with value 0 or 1).
+        After reaching (n - 1, n - 1), returning to (0, 0) by moving left or up through valid path cells.
+        When passing through a path cell containing a cherry, you pick it up, and the cell becomes an empty cell 0.
+        If there is no valid path between (0, 0) and (n - 1, n - 1), then no cherries can be collected.
+
+        Example 1.
+        Input: grid = [[0,1,-1],[1,0,-1],[1,1,1]]
+        Output: 5
+        Explanation: The player started at (0, 0) and went down, down, right right to reach (2, 2).
+        4 cherries were picked up during this single trip, and the matrix becomes [[0,1,-1],[0,0,-1],[0,0,0]].
+        Then, the player went left, up, up, left to return home, picking up one more cherry.
+        The total number of cherries picked up is 5, and this is the maximum possible.
+        Example 2.
+        Input: grid = [[1,1,-1],[1,-1,1],[-1,1,1]]
+        Output: 0
+
+        Constraints:
+
+        n == grid.length
+        n == grid[i].length
+        1 <= n <= 50
+        grid[i][j] is -1, 0, or 1.
+        grid[0][0] != -1
+        grid[n - 1][n - 1] != -1
+     */
+    public static void main(String[] args) {
+        System.out.println("Solution: " + Solution.cherryPickup(new int[][]{{0,1,-1},{1,0,-1},{1,1,1}}));
+    }
+
+    // Time: O(4^N)
+    // Space: O(N^4)
+    static class Solution {
+        static int n;
+        static int[][][][] dp;
+        public static int cherryPickup(int[][] grid) {
+            n = grid.length;
+            dp = new int[n][n][n][n];
+            int ans = recursion(0,0,0,0,grid);
+            return Math.max(ans, 0);
+        }
+        private static int recursion(int r1,int c1,int r2,int c2,int[][] grid)   {
+            if(r1>=n||c1>=n||r2>=n||c2>=n||grid[r1][c1] == -1|| grid[r2][c2] == -1)
+                return Integer.MIN_VALUE;
+            if(r1 == n-1 && c1 == n-1)
+                return grid[r1][c1];
+            if(dp[r1][c1][r2][c2] != 0)
+                return dp[r1][c1][r2][c2];
+            int cherries = 0;
+            if(r1 == r2 && c1 == c2)
+                cherries += grid[r1][c1];
+            else    cherries += grid[r1][c1] + grid[r2][c2];
+
+            int f1 = recursion(r1,c1+1,r2,c2+1,grid); // hh
+            int f2 = recursion(r1+1,c1,r2+1,c2,grid); // vv
+            int f3 = recursion(r1,c1+1,r2+1,c2,grid); // hv
+            int f4 = recursion(r1+1,c1,r2,c2+1,grid); // vh
+
+            cherries += Math.max(Math.max(f1,f2),Math.max(f3,f4));
+            return dp[r1][c1][r2][c2] = cherries;
+        }
+    }
+}
