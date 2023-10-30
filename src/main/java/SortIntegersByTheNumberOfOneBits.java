0d3c07c52b82c4576f4b2bde85b3fa631a9736e2
murodin <murat_dindar@yahoo.com>
2023-10-30 15:28:31 +0300

    Sort Integers By The Number Of One Bits implemented.

--- src/main/java/SortIntegersByTheNumberOfOneBits.java
+++ src/main/java/SortIntegersByTheNumberOfOneBits.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class CountSquareSubmatricesWithAllOnes {
+public class SortIntegersByTheNumberOfOneBits {
     /*
         You are given an integer array arr. Sort the integers in the array in ascending order by the number of 1's in their binary representation
         and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
