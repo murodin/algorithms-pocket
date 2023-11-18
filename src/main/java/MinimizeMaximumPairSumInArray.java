f82b61320ba12630d42782a589c7e5a7f2ec4d83
murodin <murat_dindar@yahoo.com>
2023-11-18 12:36:55 +0300

    Minimize Maximum Pair Sum In Array implemented.

---
+++ src/main/java/MinimizeMaximumPairSumInArray.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+public class MinimizeMaximumPairSumInArray {
+    /*
+        The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+        For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+        Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+        Each element of nums is in exactly one pair, and
+        The maximum pair sum is minimized.
+        Return the minimized maximum pair sum after optimally pairing up the elements.
+
+        Example 1:
+        Input: nums = [3,5,2,3]
+        Output: 7
+        Explanation: The elements can be paired up into pairs (3,3) and (5,2).
+        The maximum pair sum is max(3+3, 5+2) = max(6, 7) = 7.
+        Example 2:
+        Input: nums = [3,5,4,2,4,6]
+        Output: 8
+        Explanation: The elements can be paired up into pairs (3,5), (4,4), and (6,2).
+        The maximum pair sum is max(3+5, 4+4, 6+2) = max(8, 8, 8) = 8.
+
+
+        Constraints:
+
+        n == nums.length
+        2 <= n <= 105
+        n is even.
+        1 <= nums[i] <= 105
+     */
+    public static void main(String[] args) {
+        System.out.println("Solution: " + Solution.minPairSum(new int[]{3,5,4,2,4,6}));
+    }
+
+    // Time: O(NLogN)
+    // Space: O(1)
+    static class Solution {
+        public static int minPairSum(int[] nums) {
+            Arrays.sort(nums);
+            var max = 0;
+            var n = nums.length;
+            for (var i=0; i < n/2; i++)
+                max = Math.max(max, nums[i] + nums[n-i-1]);
+            return max;
+        }
+    }
+}
