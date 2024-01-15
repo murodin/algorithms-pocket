8742044b39214995a6238498c7bb15312710156e
murodin <murat_dindar@yahoo.com>
2024-01-16 00:33:37 +0300

    Minimum Number Of Steps To Make Two Strings Anagram implemented.

---
+++ src/main/java/MinimumNumberOfStepsToMakeTwoStringsAnagram.java
@@ -0,0 +1,52 @@
+public class MinimumNumberOfStepsToMakeTwoStringsAnagram {
+    /*
+        You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
+        Return the minimum number of steps to make t an anagram of s.
+        An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+
+        Example 1:
+        Input: s = "bab", t = "aba"
+        Output: 1
+        Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+        Example 2:
+        Input: s = "leetcode", t = "practice"
+        Output: 5
+        Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+        Example 3:
+        Input: s = "anagram", t = "mangaar"
+        Output: 0
+        Explanation: "anagram" and "mangaar" are anagrams.
+
+
+        Constraints:
+
+        1 <= s.length <= 5 * 104
+        s.length == t.length
+        s and t consist of lowercase English letters only.
+     */
+    public static void main(String[] args) {
+        System.out.println("Solution: " + Solution.minSteps("leetcode", "practice"));
+    }
+
+    // Time: O(N)
+    // Space: O(1)
+    static class Solution {
+        public static int minSteps(String s, String t) {
+            int [] map = new int[26];
+            for(char ch: s.toCharArray()) {
+                map[ch-'a']--;
+            }
+
+            for(char ch: t.toCharArray()) {
+                map[ch-'a']++;
+            }
+            int count = 0;
+            for(int i=0; i < 26; i++) {
+                if(map[i] > 0) {
+                    count += map[i];
+                }
+            }
+            return count;
+        }
+    }
+}
